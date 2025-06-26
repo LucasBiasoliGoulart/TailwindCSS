1072b2a1934a189e976dc8201b441b4e05e46363
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-06-26 10:38:01 -0300

    tailwind

--- src/script.js
+++ src/script.js
@@ -1,15 +1,18 @@
-// Função de Login
 function validarCampos() {
+    const nomeInput = document.getElementById('nome');
     const emailInput = document.getElementById('email');
     const senhaInput = document.getElementById('senha');
     const textoCampo = document.getElementById('texto');
     
     let emailValido = emailInput.value.includes('@');
     let senhaValida = senhaInput.value.length >= 6;
+    let textoValido = textoCampo.value.trim() !== '';
+    let nomeValido = nomeInput.value.trim() !== '';
 
     // Resetar bordas
     emailInput.classList.remove('border-red-500');
     senhaInput.classList.remove('border-red-500');
+    textoCampo.classList.remove('border-red-500');
 
     // Se inválido, adiciona borda vermelha
     if(!emailValido) {
@@ -18,7 +21,10 @@ function validarCampos() {
     if(!senhaValida) {
         senhaInput.classList.add('border-red-500')
     }
-    if(emailValido && senhaValida) {
+    if(!textoValido) {
+        textoCampo.classList.add('border-red-500');
+    }
+    if(emailValido && senhaValida && textoValido) {
         alert('Tudo certo! enviado...');
     }
 }
