90a4860a14148c6ca6ec104cb418d3f03247adda
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-06-27 08:02:31 -0300

    tailwind

--- src/script.js
+++ src/script.js
@@ -1,18 +1,14 @@
+// Login
 function validarCampos() {
-    const nomeInput = document.getElementById('nome');
     const emailInput = document.getElementById('email');
     const senhaInput = document.getElementById('senha');
-    const textoCampo = document.getElementById('texto');
     
     let emailValido = emailInput.value.includes('@');
     let senhaValida = senhaInput.value.length >= 6;
-    let textoValido = textoCampo.value.trim() !== '';
-    let nomeValido = nomeInput.value.trim() !== '';
 
     // Resetar bordas
     emailInput.classList.remove('border-red-500');
     senhaInput.classList.remove('border-red-500');
-    textoCampo.classList.remove('border-red-500');
 
     // Se inválido, adiciona borda vermelha
     if(!emailValido) {
@@ -24,7 +20,41 @@ function validarCampos() {
     if(!textoValido) {
         textoCampo.classList.add('border-red-500');
     }
-    if(emailValido && senhaValida && textoValido) {
+    if(emailValido && senhaValida) {
+        alert('Tudo certo! enviado...');
+    }
+}
+
+// Contato
+function Enviar() {
+    const nomeInput = document.getElementById('nome');
+    const email = document.getElementById('email');
+    const textoCampo = document.getElementById('texto');
+
+    let nomeValido = nomeInput.value.trim() !== '';
+    let emailCorret = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+    let textoValido = textoCampo.value.trim() !== '';
+
+    // Resetar estilos
+    [nomeInput, email, textoCampo].forEach(el => {
+        el.classList.remove('border-red-500');
+        el.classList.add('border-gray-300');
+    });
+
+    // Aplicando o vermelho se for inválido e remove cinza
+    if(!nomeValido) {
+        nomeInput.classList.add('border-red-500');
+        nomeInput.classList.remove('border-gray-300');
+    }
+    if(!emailCorret) {
+        email.classList.add('border-red-500');
+        email.classList.remove('border-gray-300');
+    }
+    if(!textoValido) {
+        textoCampo.classList.add('border-red-500');
+        textoCampo.classList.remove('border-gray-300');
+    }
+    if (nomeValido && emailValido && textoValido) {
         alert('Tudo certo! enviado...');
     }
 }
